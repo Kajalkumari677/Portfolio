df299ddd5cb2ed9939d0e60e70478ff6ce24103f
kajal kumari <49594530+Kajalkumari677@users.noreply.github.com>
2022-02-12 17:21:16 +0530

    Add files via upload

---
+++ src/portfolio.js
@@ -0,0 +1,179 @@
+//I Would Highly Recommend You To Customize The Theme According To Your Taste.
+export const globalStyles = {
+  "--grad": "linear-gradient(315deg,#48a9fe,#00cdac)", // Use gradients only
+  // "--grad": "linear-gradient(315deg, #48a9fe 0%, #734ae8 74%)", // Use gradients only
+  // "--grad": "linear-gradient(315deg, #06bcfb 0%, #4884ee 74%)",
+  "--txtcolor": "#272341",
+  "--p_color": "#00cdac",
+  "--s_color": "#fff",
+};
+
+// Navigation Bar Name
+export const navbar_name = "<Kajal>";
+
+// Main Page
+export const main = {
+  name: "Kajal Kumari",
+  doamin: "A MERN Stack Web Developer",
+  resume:
+    "https://drive.google.com/file/d/1YROSqX8oGcBi7wwl01Bqky-jDXMhT8Vg/view?usp=sharing",
+};
+
+// AboutMe
+export const aboutme = {
+  imgLink:
+    "https://avatars.githubusercontent.com/u/49594530?v=4",
+  p1: "Hey, My name is Kajal. I have completed my Bachelor of Engineering from Lovely Professional University, Phagwara(Punjab)",
+  p2: " I'm a passionate coder and an enthusiastic Web Developer capable of applying programming skills to solve complex problems. Seeking a challenging role in the field of software development that will offer an opportunity for further development of my abilities, skills and knowledge in an organization.",
+  p3: "Apart from coding I also like Designing, Crafting👨 and love travelling 🏍️.",
+};
+
+// Skills
+export const whatido = {
+  title: "Full Stack Development",
+
+  details: [
+    "Building responsive website using HTML,CSS,JS,React",
+    "Creating backend application in Node, ExpressJS",
+    "Building Simple RESTful CRUD APIs",
+    "Knowledge of MongoDB Database",
+    "Knowledge of various UI libraries",
+  ],
+  logos: [
+    {
+      name: "HTML5",
+      iconifyClassName: "vscode-icons:file-type-html", // PS : Use iconify class name
+      id: "1",
+    },
+    {
+      name: "CSS",
+      iconifyClassName: "vscode-icons:file-type-css",
+      id: "2",
+    },
+    {
+      name: "JavaScript",
+      iconifyClassName: "logos:javascript",
+      id: "3",
+    },
+    {
+      name: "ReactJS",
+      iconifyClassName: "vscode-icons:file-type-reactjs",
+      id: "4",
+    },
+    {
+      name: "Redux",
+      iconifyClassName: "logos:redux",
+      id: "5",
+    },
+    {
+      name: "NodeJS",
+      iconifyClassName: "logos:nodejs",
+      id: "6",
+    },
+    {
+      name: "ExpressJS",
+      iconifyClassName: "logos:express",
+      id: "7",
+    },
+    {
+      name: "MongoDB",
+      iconifyClassName: "logos:mongodb",
+      id: "8",
+    },
+    {
+      name: "Postman",
+      iconifyClassName: "logos:postman-icon",
+      id: "9",
+    },
+    {
+      name: "npm",
+      iconifyClassName: "logos:npm-icon",
+      id: "10",
+    },
+    {
+      name: "GIT",
+      iconifyClassName: "logos:git",
+      id: "11",
+    },
+    {
+      name: "Material-UI",
+      iconifyClassName: "logos:material-ui",
+      id: "12",
+    },
+    {
+      name: "Ant Design",
+      iconifyClassName: "logos:ant-design",
+      id: "13",
+    },
+    {
+      name: "Netlify",
+      iconifyClassName: "vscode-icons:file-type-netlify",
+      id: "14",
+    },
+    {
+      name: "Vercel",
+      iconifyClassName: "logos:vercel-icon",
+      id: "15",
+    },
+  ],
+};
+
+// Project Details
+export const details = [
+  {
+    id: 1,
+    title: "Practo-Clone",
+    desc: `An online application for booking appointments and consultations
+    with doctors.`,
+    feature: `Sign-in/Sign-up,
+    Book appointments, tests or consultations,
+    Can easily purchase medicines,
+    Checkout and Payment Gateway`,
+    tech: `Tech Stack: HTML | CSS | JavaScript`,
+    img: "https://i.postimg.cc/MTdcSd9G/kajal2.png",
+    github: "https://github.com/Kajalkumari677/practo",
+    link: "https://practo-lake.vercel.app/",
+
+  },
+  {
+    id: 2,
+    title: "Homedepot-Clone",
+    desc: `A website that supplies construction tools, office furniture, and
+    services..`,
+    feature: `Features:
+    Sign-in/Sign-up, User Authentication,
+    Laid out the database schema and build the REST API for the same
+    Navbar,
+    Carousel,
+    Add to cart & Check out.`,
+    tech: `Tech Stack: HTML | CSS | JavaScript |EJS |MongoDB Atlas | ExpressJS | NodeJS `,
+    img: "https://i.postimg.cc/HxCn5Fhn/kajal1.png",
+    github: "https://github.com/Kajalkumari677/homedepot",
+    link: "https://homedepot-rho.vercel.app/",
+  },
+  {
+    id: 3,
+    title: "Zappos-Clone",
+    desc: `A website that deals with the sale of American online shoes and
+    clothing items.`,
+    feature: `Built landing page including fixed navbar, footer and slideshow,
+    Added specific section for every product,
+    Applied sorting, filtering by color, and prices for every product,
+    `,
+    tech: `Tech Stack: HTML | CSS | JavaScript .`,
+    img: "https://i.postimg.cc/Xvqp4wyN/kajal3.png",
+    link: "https://zapposproject.vercel.app/",
+    github: "https://github.com/Kajalkumari677/ZapposClone",
+  },
+  
+];
+
+// Social Media Links
+export const links = [
+  
+  "https://www.linkedin.com/in/kajal-kumari-6847b0148/",
+  "https://api.whatsapp.com/send/?phone=9304163858&text&app_absent=0",
+  "mailto:677kajal@gmail.com",
+  "https://hashnode.com/@kajalkumari677",
+  "https://github.com/Kajalkumari677",
+];
